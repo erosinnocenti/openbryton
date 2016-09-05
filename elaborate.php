c3092d8fdc66782aa7b51b97826fbb42bb14b7a2
Eros Innocenti <eros@newtechweb.it>
2016-09-05 22:03:39 +0200

    Added disclaimer

--- elaborate.php
+++ elaborate.php
@@ -1,4 +1,19 @@
 <?
+	// Copyright (C) 2012, 2016 Eros Innocenti
+	// This file is part of OpenBryton.
+	//
+	// OpenBryton is free software: you can redistribute it and/or modify
+	// it under the terms of the GNU General Public License as published by
+	// the Free Software Foundation, either version 3 of the License, or
+	// (at your option) any later version.
+	//
+	// OpenBryton is distributed in the hope that it will be useful,
+	// but WITHOUT ANY WARRANTY; without even the implied warranty of
+	// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	// GNU General Public License for more details.
+	//
+	// You should have received a copy of the GNU General Public License
+
 	header('Access-Control-Allow-Origin: http://openrouteservice.org'); 
 	require_once('utils.php');
 	error_reporting(E_ALL);
